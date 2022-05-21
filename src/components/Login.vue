6d687178fa3d1f0169635deb4f22ee1b094a7864
solohad1n <fox663399@gmail.com>
2022-05-21 21:27:26 +0600

    separation of entry and registration

---
+++ src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+	<form @submit.prevent="handleSubmit">
+		<input v-model="email" type="email" required placeholder="Email" />
+		<input v-model="password" type="password" required placeholder="Password" />
+		<button>Log In</button>
+	</form>
+</template>
+
+<script setup>
+  import { ref } from "@vue/reactivity";
+  const email = ref("");
+  const password = ref("");
+
+  const handleSubmit = () => {
+  console.log({
+    email:email.value,
+    password:password.value,
+  })
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,17 +1,54 @@
+<script setup>
+import SignUpForm from "../components/SignUp.vue";
+import Login from "../components/Login.vue";
+import { ref } from "@vue/reactivity";
+
+
+const showForm = ref(true)
+</script>
+
 <template>
   <div class="welcome container">
     <p>Welcome</p>
-    <SignUpForm/>
+    <div v-if="showForm">
+      <h2>Sign Up</h2>
+      <SignUpForm/>
+    </div>
+    <div v-else>
+      <h2>Login</h2>
+      <Login/>
+    </div>
   </div>
 </template>
 
-<script setup>
-import SignUpForm from "../components/SignUp.vue";
-</script>
-
 <style>
-.welcome{
+.welcome {
   text-align: center;
   padding: 20px 0;
+}
+.welcome form {
+  width: 300px;
+  margin: 20px auto;
+}
+.welcome label {
+  display: block;
+  margin: 20px 0 10px;
+}
+.welcome input {
+  width: 100%;
+  padding: 10px;
+  border-radius: 20px;
+  border: 1px solid #c4c4c4;
+  outline: none;
+  color: #999;
+  margin: 10px auto;
+}
+.welcome span {
+  font-weight: bold;
+  text-decoration: underline;
+  cursor: pointer;
+}
+.welcome button {
+  margin: 20px auto;
 }
 </style>
