f3d7b0866a2198de77c24d801ee7336edf2e47d5
solohad1n <fox663399@gmail.com>
2022-05-21 16:33:53 +0600

    SignUp component template creation

---
+++ src/components/SignUp.vue
@@ -0,0 +1,29 @@
+<template>
+  <form>
+    <input v-model="name" type="text" required placeholder="Name" />
+    <input v-model="email" type="email" required placeholder="Email" />
+    <input v-model="password" type="password" required placeholder="Password" />
+    <button>Sign Up</button>
+  </form>
+</template>
+
+<script setup>
+import {ref} from '@vue/reactivity'
+
+const name = ref('');
+const email = ref('');
+const password = ref('');
+
+const handleSubmit = () => {
+  console.log({
+    name:name.value,
+    email:email.value,
+    password:password.value,
+  })
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="welcome container">
+    <p>Welcome</p>
+    <SignUpForm/>
+  </div>
+</template>
+
+<script setup>
+import SignUpForm from "../components/SignUp.vue";
+</script>
+
+<style>
+.welcome{
+  text-align: center;
+  padding: 20px 0;
+}
+</style>
